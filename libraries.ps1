39cf813c18f5533d824e70e5b0b598aaa906ff3a
Marcin Skic <marcinskicoficjal@gmail.com>
2023-02-05 17:44:41 +0100

    Create script for easy date selection (borrowed from docs)

---
+++ libraries.ps1
@@ -0,0 +1,48 @@
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+function GetDateUsingCalendar($title){
+    if(-not $title){
+        $title = 'Select a Date'
+    }
+
+    $form = New-Object Windows.Forms.Form -Property @{
+        StartPosition = [Windows.Forms.FormStartPosition]::CenterScreen
+        Size          = New-Object Drawing.Size 243, 230
+        Text          = $title
+        Topmost       = $true
+    }
+    
+    $calendar = New-Object Windows.Forms.MonthCalendar -Property @{
+        ShowTodayCircle   = $false
+        MaxSelectionCount = 1
+    }
+    $form.Controls.Add($calendar)
+    
+    $okButton = New-Object Windows.Forms.Button -Property @{
+        Location     = New-Object Drawing.Point 38, 165
+        Size         = New-Object Drawing.Size 75, 23
+        Text         = 'OK'
+        DialogResult = [Windows.Forms.DialogResult]::OK
+    }
+    $form.AcceptButton = $okButton
+    $form.Controls.Add($okButton)
+    
+    $cancelButton = New-Object Windows.Forms.Button -Property @{
+        Location     = New-Object Drawing.Point 113, 165
+        Size         = New-Object Drawing.Size 75, 23
+        Text         = 'Cancel'
+        DialogResult = [Windows.Forms.DialogResult]::Cancel
+    }
+    $form.CancelButton = $cancelButton
+    $form.Controls.Add($cancelButton)
+    
+    $result = $form.ShowDialog()
+    
+    if ($result -eq [Windows.Forms.DialogResult]::OK) {
+        $date = $calendar.SelectionStart
+        return $date
+    } else {
+        return $False
+    }
+}
